94ebeafdeadd3f7304d529cad6ef91d35586d5db
Claudio Meira <claudiomeirabs@gmail.com>
2022-05-13 22:21:15 -0300

    Adiciona interface Data Access Objectc.

---
+++ Interfaces/IDao.cs
@@ -0,0 +1,8 @@
+﻿namespace Desafio_07.Interfaces;
+
+public interface IDao<T> where T : class
+{
+    public Task<T> FindById(int id);
+    public Task<IEnumerable<T>> FindAll();
+    public Task Create(T data);
+}
