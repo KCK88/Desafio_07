b53174dd95a4742b57d1f5acc17e2b95330c29b5
Claudio Meira <claudiomeirabs@gmail.com>
2022-05-14 22:37:43 -0300

    Adiciona rascunho de arquivo inicial

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.AspNetCore.Builder.Internal;
+using MySqlConnector;
+
 Console.WriteLine("Hello, World!");
+
+var builder = ApplicationBuilder.
+
+
+
+var connection =
+    new MySqlConnection(
+        "server=localhost; port=3306; database=BancoEscola; user=root; password=8617509; Persist Security Info=False; Connect Timeout=300;");
