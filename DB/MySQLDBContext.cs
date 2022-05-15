a0afdb4238149d4fa9c5055d43cf24b9b12eca58
Claudio Meira <claudiomeirabs@gmail.com>
2022-05-14 22:37:11 -0300

    Adiciona configuração MySQL

---
+++ DB/MySQLDBContext.cs
@@ -0,0 +1,56 @@
+﻿using Desafio_07.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Desafio_07.DB;
+
+public class MySQLDBContext : DbContext
+{
+    public DbSet<Aluno> Alunos { get; set; }
+    public DbSet<AlunoCurso> AlunoCursos { get; set; }
+    public DbSet<Curso> Cursos { get; set; }
+
+    public MySQLDBContext(DbContextOptions<MySQLDBContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // aluno faz curso
+        //      n - n
+        modelBuilder.Entity<Aluno>().ToTable("Alunos");
+        modelBuilder.Entity<Aluno>().HasKey(aluno => aluno.Id).HasName("Id");
+        modelBuilder.Entity<Aluno>().Property(aluno => aluno.Id).HasColumnName("Id").HasColumnType("int").UseMySqlIdentityColumn().IsRequired();
+        modelBuilder.Entity<Aluno>().Property(aluno => aluno.Nome).HasColumnName("Nome").HasColumnType("varchar(255)").IsRequired();
+        modelBuilder.Entity<Aluno>().Property(aluno => aluno.Idade).HasColumnName("Idade").HasColumnType("int").IsRequired();
+
+        modelBuilder.Entity<Curso>().ToTable("Cursos");
+        modelBuilder.Entity<Curso>().HasKey(curso => curso.Id).HasName("Id");
+        modelBuilder.Entity<Curso>().Property(curso => curso.Id).HasColumnName("Id").HasColumnType("int").UseMySqlIdentityColumn().IsRequired();
+        modelBuilder.Entity<Curso>().Property(curso => curso.Nome).HasColumnName("Nome").HasColumnType("varchar(255").IsRequired();
+
+        modelBuilder.Entity<AlunoCurso>().ToTable("AlunoCursos");
+        modelBuilder.Entity<AlunoCurso>().HasKey(alunoCurso => alunoCurso.IdAluno).HasName("IdAluno");
+        modelBuilder.Entity<AlunoCurso>().HasKey(alunoCurso => alunoCurso.IdCurso).HasName("IdCurso");
+        modelBuilder.Entity<AlunoCurso>().Property(alunoCurso => alunoCurso.Status).HasColumnName("Status").HasColumnType("varchar(255").IsRequired();
+        modelBuilder.Entity<AlunoCurso>().Property(alunoCurso => alunoCurso.Nota).HasColumnName("Nota").HasColumnType("int").IsRequired();
+
+        modelBuilder
+            .Entity<AlunoCurso>()
+            .HasOne<Aluno>()
+            .WithMany()
+            .HasPrincipalKey(aluno => aluno.Id)
+            .HasForeignKey(alunoCurso => alunoCurso.IdAluno)
+            .OnDelete(DeleteBehavior.NoAction)
+            .HasConstraintName("FK_Aluno_AlunoCurso");
+        
+        modelBuilder
+            .Entity<AlunoCurso>()
+            .HasOne<Curso>()
+            .WithMany()
+            .HasPrincipalKey(curso => curso.Id)
+            .HasForeignKey(alunoCurso => alunoCurso.IdCurso)
+            .OnDelete(DeleteBehavior.NoAction)
+            .HasConstraintName("FK_Curso_AlunoCurso");
+    }
+}
---
+++ script.sql
@@ -0,0 +1,24 @@
+﻿create database BancoEscola;
+
+create table Alunos(
+                       Id int,
+                       Nome varchar(255) not null,
+                       Idade int not null,
+                       PRIMARY KEY (Id)
+);
+
+create table Cursos(
+                       Id int,
+                       Nome varchar(255) not null ,
+                       PRIMARY KEY (Id)
+);
+
+create table AlunoCursos(
+                            IdAluno int,
+                            IdCurso int,
+                            Status varchar(255) not null,
+                            Nota int not null,
+                            PRIMARY KEY (IdAluno, IdCurso),
+                            FOREIGN KEY (IdAluno) references Alunos(Id),
+                            FOREIGN KEY (IdCurso) references Cursos(Id)
+);
